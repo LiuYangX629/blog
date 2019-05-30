94ca819532fef280c69e8dd34eced59c4038e42b
LiuXY1998 <1409824833@qq.com>
2019-05-30 17:17:17 +0800

    2019/5/30 17:17

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 window.onload = function () {
     let home =document.getElementById("home");
     let btnList = document.getElementsByClassName("btnList");
-    let bannerPointer = btnList[0].getElementsByTagName("li");
     let activeColor ="#00c1de" , disactiveColor = "#fff";
     console.log(home);
 
@@ -18,17 +17,7 @@ window.onload = function () {
         home.style.color = "#ffffff";
     };
 
-    for(var i=0;i<bannerPointer.length;i++){
-        bannerPointer[i].onmouseenter = function () {
-            this.style.backgroundColor=activeColor;
-        };
-        bannerPointer[i].onmouseleave =function () {
-            this.style.backgroundColor=disactiveColor;
-        }
-        bannerPointer[i].onclick = function () {
-            rightBtn.onclick();
-        }
-    }
+
 
     let diaryList = document.getElementsByClassName("diaryList")[0];
     let listLi= diaryList.getElementsByTagName('li');
@@ -74,7 +63,19 @@ window.onload = function () {
     })(i)
 }
 
-
+        //轮播点
+    let bannerPointer = btnList[0].getElementsByTagName("li");
+    for(var i=0;i<bannerPointer.length;i++){
+        bannerPointer[i].onmouseenter = function () {
+            this.style.backgroundColor=activeColor;
+        };
+        bannerPointer[i].onmouseleave =function () {
+            this.style.backgroundColor=disactiveColor;
+        }
+        // bannerPointer[i].onclick = function () {
+        //     rightBtn.onclick();
+        // }
+    }
     //轮播图 bannerImg
     /*
     * index 保存窗口中显示图片下标
@@ -94,10 +95,14 @@ window.onload = function () {
         bannerImg.forEach(function (ele) {
             ele.style.zIndex=1;
         });
+        Array.prototype.forEach.call(bannerPointer,function (elem) {
+            elem.classList.remove("hot");
+        });
+        bannerPointer[index].classList.add("hot");
         bannerImg[index].style.zIndex=99;
     };
 
-    setInterval(rightBtn.onclick=function () {
+        rightBtn.onclick=function () {
         index++;
         if(index==bannerImg.length){
             index=0;
@@ -105,9 +110,14 @@ window.onload = function () {
         bannerImg.forEach(function (ele) {
             ele.style.zIndex = 1;
         });
+        Array.prototype.forEach.call(bannerPointer,function (elem) {
+            elem.classList.remove("hot");
+        });
+        bannerPointer[index].classList.add("hot");
         bannerImg[index].style.zIndex=99;
-    },5000)
+    }
     //每隔5000毫秒执行一次
+    let t = setInterval(rightBtn.onclick,1000);
 /*               end              */
 
     /*    btnList     */
@@ -120,7 +130,34 @@ window.onload = function () {
     //     };
     // }
 
+    /*
+    * 轮播移入和移出（鼠标在图上时停止轮播）
+    * */
+    let bannerLeft = document.querySelector('.bannerLeft');
+    bannerLeft.onmouseenter=function () {
+        clearInterval(t);
+    };
+    bannerLeft.onmouseleave = function () {
+        t= setInterval(rightBtn.onclick,1000);
+    };
 
+    for(var i=0;i<bannerPointer.length;i++){
+        bannerPointer[i].lxy=i;
+        bannerPointer[i].onclick=function () {
+            index=this.lxy;
+            Array.prototype.forEach.call(bannerPointer,function (elem) {
+                elem.classList.remove("hot");
+            });
+            bannerImg.forEach(function (ele) {
+                ele.style.zIndex=1;
+            });
+            this.classList.add("hot");
+            // console.log(i);
+            bannerImg[this.lxy].style.zIndex=99;
+
+
+        }
+    }
 
 };
 
