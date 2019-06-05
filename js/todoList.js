6104fc602db383acffd824d02e22a2c2b5534873
LiuXY1998 <1409824833@qq.com>
2019-06-05 11:09:54 +0800

    2019/6/5 11:09

--- js/todoList.js
+++ js/todoList.js
@@ -2,18 +2,20 @@ window.addEventListener('load',function () {
     let tab =document.querySelectorAll(".tab>li");
     let prev =0;
     let content =document.querySelector(".content");
+    let type ="all";
+    let flag ={all:'all',done:true,doing:false};
     let todolist =[
         {
             id:1, content:"端午节要交作业" , ctime:"2019/5/29" ,status:false
         },
         {
-            id:2, content:"不想交作业" , ctime:"2019/5/30" ,status:false
+            id:4, content:"不想交作业" , ctime:"2019/6/12" ,status:false
         },
         {
             id:3, content:"企业文档" , ctime:"2019/6/10" ,status:true
         },
         {
-            id:4, content:"端午节要交作业" , ctime:"2019/6/12" ,status:false
+            id:2, content:"不想交作业" , ctime:"2019/5/30" ,status:false
         }
     ];
 
@@ -22,21 +24,10 @@ window.addEventListener('load',function () {
             tab[prev].classList.remove('hot');
             this.classList.add('hot');
             prev=index;
-            let type =this.getAttribute('type');
-            let arr =[];
-            
-            switch (type) {
-                case 'all':
-                    arr = todolist;
-                    break;
-                case 'done':
-                    arr =todolist.filter(function (ele) {return ele.status;});
-                    break;
-                case 'doing':
-                    arr = todolist.filter(function (ele) {return !ele.status});
-                    break;
-            }
-            render(arr);
+
+            type =this.getAttribute('type');
+
+            render(filterData(type));
         }
     });
     tab[0].onclick();
@@ -47,6 +38,37 @@ window.addEventListener('load',function () {
     // });
     // render(doing);
 
+
+    content.onclick =function (e) {
+        let arr=[];
+      let target =e.target;
+      let id = target.parentNode.id;
+
+        if(target.nodeName==="INPUT"){
+            // console.log(1);
+        }else if(target.nodeName==="DEL"){
+            let index =todolist.findIndex(ele=>ele.id===id);
+            todolist.splice(index,1);
+            render(filterData(type));
+        }
+    };
+
+    function filterData(type) {
+        let arr=[];
+        switch (type){
+            case "all":
+              arr=todolist;
+              break;
+            case "done":
+               arr=todolist.filter(ele=>ele.status);
+               break;
+            case "doing":
+                arr =todolist.filter(ele=>!ele.status);
+                break;
+        }
+        return arr;
+    }
+
     //渲染列表
     function render(arr) {
         let html = "";
@@ -54,13 +76,20 @@ window.addEventListener('load',function () {
             if(elem.status){
                 html+=`
             <li>
-            <input type="checkbox" checked> <p>${elem.content}</p> <time>${elem.ctime}</time>
+            <input type="checkbox" checked>
+             <p>${elem.content}</p>
+             <del>X</del>
+             <time>${elem.ctime}</time>
+
         </li>
             `;
             }else{
                 html+=`
             <li>
-            <input type="checkbox"> <p>${elem.content}</p> <time>${elem.ctime}</time>
+            <input type="checkbox"> 
+            <p>${elem.content}</p>
+                         <del>X</del>
+             <time>${elem.ctime}</time>
         </li>
             `;
             }
